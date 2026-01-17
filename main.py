69cb6c45672b1b9546c7892d05faad53f4f18426
Amitkumar2801 <amitkumar.arwal28@gmail.com>
2026-01-17 22:19:10 +0530

    code

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import phonenumbers
 from myphone import number
 from phonenumbers import geocoder, carrier
-from opencage.geocoder import OpenCageGeocode
 
-# 1. Phone number details nikalna
+from opencage.geocoder import OpenCageGeocode # Line 4: Sahi spelling ke saath
+
+# 1. Phone details
 pepnumber = phonenumbers.parse(number)
 location = geocoder.description_for_number(pepnumber, "en")
 print("Location: ", location)
 
-service_pro = carrier.name_for_number(pepnumber, "en")
-print("Service Provider: ", service_pro)
-
-# 2. OpenCage se Exact Map Location nikalna
-# Yahan apni API Key quotes (" ") ke andar likhein
-key = "APKI_OPENCAGE_API_KEY_YAHAN_DAALEIN" 
-
-geocoder_osm = OpenCageGeocode(key)
+# 2. OpenCage Setup
+key = "APNI_API_KEY_YAHAN_DALO" # Dashboard se copy karke yahan paste karein
+coder = OpenCageGeocode(key)
 query = str(location)
-results = geocoder_osm.geocode(query)
+results = coder.geocode(query)
 
-# Result print karein
-if results:
-    lat = results[0]['geometry']['lat']
-    lng = results[0]['geometry']['lng']
-    print(f"Latitude: {lat}, Longitude: {lng}")
-else:
-    print("Location nahi mili")
+# Latitude aur Longitude nikalna
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat, lng)
--- myphone.py
+++ myphone.py
@@ -1,2 +1,2 @@
 #this file myphone.py hai jisme aap apna phone number daalenge
-number = "yeha apna phone number daalein with country code"
+number = "+911234567890"
